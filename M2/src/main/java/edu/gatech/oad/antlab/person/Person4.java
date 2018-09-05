2e4aac5de75eed64c81eebff419fe8048171f7f6
Lwilson67gatech <claywilson99@att.net>
2018-09-04 21:22:31 -0400

    Added name and gtid

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -5,7 +5,7 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  modified string 
  *  
- *  @author Clay
+ *  @author Lewey C. Wilson
  *  @version 1.1
  */
 public class Person4 {
